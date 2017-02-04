a8ae8d8f877059187992c595462a8f098222de79
JangKyungJoo <rudwn826@gmail.com>
2017-02-04 21:01:20 +0900

    admin page commit

--- Backend/libs/dbConnect.js
+++ Backend/libs/dbConnect.js
@@ -5,6 +5,9 @@ var mysql = require('mysql');
 
 exports.dbConnect = function() {
     return connection = mysql.createConnection({
-
+      host: 'aws-rds-bk.cdinvyqu4zcg.ap-northeast-2.rds.amazonaws.com',
+              user: 'user',
+              password: 'dho1921!036',
+              database: 'no_show'
     });
 };
