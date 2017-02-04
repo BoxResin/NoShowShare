63cd75193398e115a51fc4c20d8c3a48d09d7afb
lemonyun <minhyun1102@gmail.com>
2017-02-04 21:32:07 +0900

    reservation alert 구현

--- Android-Client/app/src/main/java/unithon/contest/noshowshare/MyReservationActivity.java
+++ Android-Client/app/src/main/java/unithon/contest/noshowshare/MyReservationActivity.java
@@ -3,6 +3,7 @@ package unithon.contest.noshowshare;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 
 /**
  * Created by minhyeon on 2017-02-04.
@@ -18,5 +19,16 @@ public class MyReservationActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_reserve:
+
+                WarningReservationDialog dialog = new WarningReservationDialog(this);
+                WindowManager.LayoutParams wm = dialog.getWindow().getAttributes();
+                wm.copyFrom(dialog.getWindow().getAttributes());
+                wm.width = 1200;
+                wm.height = 1700;
+                dialog.getWindow().setAttributes(wm);
+                dialog.show();
+        }
     }
 }
