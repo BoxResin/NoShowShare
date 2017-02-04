9acf8dd854fb0e2fd5cf5f5a238abf7b2bfbbfc4
BoxResin <boxresin@gmail.com>
2017-02-04 16:00:25 +0900

    예약 리스트 항목 디자인 수정

--- Android-Client/app/src/main/java/unithon/contest/noshowshare/MyReservationActivity.java
+++ Android-Client/app/src/main/java/unithon/contest/noshowshare/MyReservationActivity.java
@@ -2,19 +2,16 @@ package unithon.contest.noshowshare;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 /**
  * Created by minhyeon on 2017-02-04.
  */
 
-public class ReservationDetailActivity extends AppCompatActivity {
+public class MyReservationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation_detail);
-        setSupportActionBar((Toolbar)findViewById(R.id.toolBar));
-
 
     }
 }
