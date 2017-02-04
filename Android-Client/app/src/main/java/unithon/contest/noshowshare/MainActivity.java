f436ddcabee1e6e65c5498d2fc415b1dc52b2185
BoxResin <boxresin@gmail.com>
2017-02-04 17:06:39 +0900

    지역 선택 스피너 완성

--- Android-Client/app/src/main/java/unithon/contest/noshowshare/MainActivity.java
+++ Android-Client/app/src/main/java/unithon/contest/noshowshare/MainActivity.java
@@ -5,6 +5,8 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import unithon.contest.noshowshare.databinding.ActivityMainBinding;
 
@@ -17,6 +19,31 @@ public class MainActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+		
+		AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				String firstArea_str = binding.spinFirstArea.getSelectedItem().toString();
+				String secondArea_str = binding.spinSecondArea.getSelectedItem().toString();
+				String thirdArea_str = binding.spinThirdArea.getSelectedItem().toString();
+
+				if (!firstArea_str.equals("시/도") && !secondArea_str.equals("시/군/도") && !thirdArea_str.equals("동/읍/면"))
+				{
+					Toast.makeText(MainActivity.this, parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+
+			}
+		};
+		binding.spinFirstArea.setOnItemSelectedListener(itemSelectedListener);
+		binding.spinSecondArea.setOnItemSelectedListener(itemSelectedListener);
+		binding.spinThirdArea.setOnItemSelectedListener(itemSelectedListener);
 	}
 
 	public void onClick(View view)
